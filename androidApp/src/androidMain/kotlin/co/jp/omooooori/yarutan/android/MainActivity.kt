b0364ea0968ff91bc974af5af107d51fd7df4cdd
omooooori <omooooori432@gmail.com>
2025-02-10 22:44:38 +0900

    build fix.

--- androidApp/src/androidMain/kotlin/co/jp/omooooori/yarutan/android/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/co/jp/omooooori/yarutan/android/MainActivity.kt
@@ -1,6 +1,6 @@
 package co.jp.omooooori.yarutan.android
 
-import mainView
+import MainView
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            mainView(application)
+            MainView(application)
         }
     }
 }
